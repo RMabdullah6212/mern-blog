bef49becc4a00463b09539232e18985c952307f2
RMabdullah6212 <abdullahaslam6212@gmail.com>
2024-08-14 11:07:17 +0500

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-export default function App() {
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+
+function App() {
   return (
-    <div>
-      apppp
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 }
+
+
+export default App;
