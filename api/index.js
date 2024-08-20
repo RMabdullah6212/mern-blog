7dfe60bf57903d5920978c0b1e3cf7868f2ef95b
RMabdullah6212 <abdullahaslam6212@gmail.com>
2024-08-20 06:23:07 +0500

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-app.listen(2500, () =>{
-console.log('server on 2500 fghj')
-});
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  const PORT = process.env.PORT;
+  const app = express();
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
